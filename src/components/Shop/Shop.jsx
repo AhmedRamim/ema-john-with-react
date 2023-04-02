e15d747d175cb6ef6ed549860e0e88a4136a53f7
ahmed <ahmedramim821@gmail.com>
2023-04-02 16:34:08 +0600

    retrive product from stored cart and set it to cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -7,22 +7,39 @@ const Shop = () => {
     const [products,setProducts] = useState([]);
     const [cart,setCart] = useState([])
 
-    const handleAddToCart = (product) => {
-        const newCart = [...cart,product]
-        setCart(newCart)
-        addToDb(product.id)
-    }
-    
+   
     useEffect(() => {
         fetch('products.json')
             .then(res=> res.json())
             .then(data => setProducts(data))
     },[])
 
-    useEffect(() => {
-        const storedCart = getShoppingCart()
-        console.log(storedCart);
-    },[])
+    useEffect(()=> {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step:1 get id of the addedProduct..........
+        for (const id in storedCart){
+            // step:2 get product from the products state by using id.....
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // step:3 add quantity...
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+
+                // step:4 add the added Product to the savedCart.
+                savedCart.push(addedProduct);
+            }
+        }
+        // step:5 set the cart.
+        setCart(savedCart)
+    },[products])
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart,product]
+        setCart(newCart)
+        addToDb(product.id)
+    }
+    
 
     return (
        <div className="shop-container  grid grid-cols-1 md:grid-cols-12 mt-8 px-3 ">
