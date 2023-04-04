50578368553739f86c5cc2073c86f8e1ad778700
ahmed <ahmedramim821@gmail.com>
2023-04-04 14:16:49 +0600

    calculation of shopping cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
-    console.log(cart);
+    let quantity = 0;
     let shipping = 0;
     let total = 0;
     for (const product of cart){
-        total = total + product.price;
+        product.quantity = product.quantity || 1;
+        total = total + (product.price * product.quantity);
         shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = total * 7 /100;
-    const grandTotal = shipping + total + tax
+    const grandTotal = shipping + total + tax;
 
     return (
         <div className='space-y-3  sticky top-0'>
             <h2 className='text-2xl font-bold text-center'>Order Summary</h2>
-            <h1 className='text-xl'>Selected Items: <span className='font-bold'>{cart.length} </span></h1>
+            <h1 className='text-xl'>Selected Items: <span className='font-bold'>{quantity} </span></h1>
             <p>Total Price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
