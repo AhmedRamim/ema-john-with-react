448d0e1e6bbcc3734b0ce5b26cbd6d4a39a5e36a
ahmed <ahmedramim821@gmail.com>
2023-04-08 22:07:02 +0600

    add router path

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import image from '../../images/Logo.svg'
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='bg-[#1C2B35] w-full  md:px-16   overflow-x-auto'>
             <div className="navbar text-white">
                 <div className="flex-1">
-                    <img className="btn btn-ghost normal-case text-xl w-full" src={image} alt="" />
+                    <img className="btn btn-ghost normal-case text-xl " src={image} alt="" />
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal px-3">
-                        <li className='hover:text-orange-400'><a>Order</a></li>
-                        <li className='hover:text-orange-400'><a>Order Review</a></li>
-                        <li className='hover:text-orange-400'><a>Manage Inventory</a></li>
-                        <li className='hover:text-orange-400'><a>Login</a></li>
+                        <Link className='mr-6 hover:text-orange-300 font-semibold' to='/'>Shop</Link>
+                        <Link className='mr-6 hover:text-orange-300 font-semibold' to='/orders'>Order Review</Link>
+                        <Link className='mr-6 hover:text-orange-300 font-semibold' to='/inventory'>Manage Inventory</Link>
+                        <Link className='mr-6 hover:text-orange-300 font-semibold' to='/login'>Login</Link>
                     </ul>
                 </div>
             </div>
